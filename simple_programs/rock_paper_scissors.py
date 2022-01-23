4fddc0a386a14cf0b626fb68422ab046d154ceaf
ianachiapco091 <ianachiapco092@gmail.com>
2022-01-23 08:27:35 +0800

    Update rock_paper_scissors.py

--- simple_programs/rock_paper_scissors.py
+++ simple_programs/rock_paper_scissors.py
@@ -2,8 +2,8 @@ import random, sys
 
 print("Rock! Paper! Scissors!")
 
-# These variables store and will be used for the game
-
+# These variables store and will be use in the game (loop)
+# These three variables indicate the results
 win = 0
 lose = 0
 tie = 0
@@ -11,94 +11,92 @@ tie = 0
 print("~~ -{ WINS: ", win, "LOSSES: ", lose, "TIES: ", tie, "}- ~~")
 
 while True:
-    print("""
+    print("""          
 'r' for ROCK, 'p' for PAPER, 's' for SCISSORS, or 'q' for QUIT
     """)
-
-    # To enter Player's Turn
-
+    
     action = str(input("==> "))
-
+    
     if action == 'r':
-
+        
         gestu_play = "ROCK"
         print(gestu_play)
-
+        
     elif action == 'p':
-
+        
         gestu_play = "PAPER"
         print(gestu_play)
-
+        
     elif action == 's':
-
+        
         gestu_play = "SCISSORS"
         print(gestu_play)
 
     elif action == 'q':
         sys.exit()
-
+        
     print("versus...")
-
+    
     # To display the Computer's Turn
-
+        
     computer_act = ['r', 'p', 's']
     computer_resp = random.choice(computer_act)
-
+        
     if computer_resp == 'r':
-
+        
         gestu_comp = "ROCK"
         print(gestu_comp)
-
+        
     elif computer_resp == 'p':
-
+        
         gestu_comp = "PAPER"
         print(gestu_comp)
-
+        
     elif computer_resp == 's':
-
+        
         gestu_comp = "SCISSORS"
         print(gestu_comp)
-
+        
     # To display the results (wins, losses, ties)
-
+    
     if gestu_play == gestu_comp:
-
+        
         print("TIE!")
         tie = tie + 1
         print("WINS: ", win, "LOSSES: ", lose, "TIES: ", tie)
-
+        
     elif gestu_play == 'ROCK' and gestu_comp == 'SCISSORS':
-
+        
         print("PLAYER WINS!")
         win = win + 1
         print("WINS: ", win, "LOSSES: ", lose, "TIES: ", tie)
-
+        
     elif gestu_play == 'PAPER' and gestu_comp == 'ROCK':
-
+        
         print("PLAYER WINS!")
         win = win + 1
         print("WINS: ", win, "LOSSES: ", lose, "TIES: ", tie)
-
+        
     elif gestu_play == 'SCISSORS' and gestu_comp == 'PAPER':
-
+        
         print("PLAYER WINS!")
         win = win + 1
         print("WINS: ", win, "LOSSES: ", lose, "TIES: ", tie)
-
+        
     elif gestu_play == 'ROCK' and gestu_comp == 'PAPER':
-
+        
         print("COMPUTER WINS!")
         lose = lose + 1
         print("WINS: ", win, "LOSSES: ", lose, "TIES: ", tie)
-
+        
     elif gestu_play == 'PAPER' and gestu_comp == 'SCISSORS':
-
+        
         print("COMPUTER WINS!")
         lose = lose + 1
         print("WINS: ", win, "LOSSES: ", lose, "TIES: ", tie)
-
+        
     elif gestu_play == 'SCISSORS' and gestu_comp == 'ROCK':
-
+        
         print("COMPUTER WINS!")
         lose = lose + 1
         print("WINS: ", win, "LOSSES: ", lose, "TIES: ", tie)
