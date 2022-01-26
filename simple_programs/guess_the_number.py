8e5819821099e87f3798aedb604e1ba83f6880ac
ianachiapco091 <ianachiapco092@gmail.com>
2022-01-26 19:11:28 +0800

    Update guess_the_number.py

--- simple_programs/guess_the_number.py
+++ simple_programs/guess_the_number.py
@@ -1,9 +1,19 @@
-import random
+import random, sys
+
+class Exit:
+    def exit_method(self):
+        
+        print("Type anything to exit")
+        speeq = input(">> ")
+        if speeq == speeq:
+            sys.exit()
 
 player_num = 0
 secret_num = random.randint(1, 100)
 al_guess = 0
 rem_guess = 15
+
+menu = Exit()
 
 print("""
 I am thinking of a number between 1 to 100...
@@ -20,15 +30,16 @@ while al_guess <= rem_guess:
         if al_guess == rem_guess:
             print("I am so sorry, you didn't guess that number...")
             print("And that number is: ", secret_num, "... Better luck next time...")
+            menu.exit_method()
             
-    
     elif player_num > secret_num:
         print("Well, but your guess number is higher...")
         
         if al_guess == rem_guess:
             print("I am so sorry, you didn't guess that number...")
             print("And that number is: ", secret_num, "... Better luck next time...")
+            menu.exit_method()
         
     elif player_num == secret_num:
         print("You finally guess that number at " , al_guess, " guesses!")
-        break
+        menu.exit_method()
